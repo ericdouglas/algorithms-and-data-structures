22265127e4dbd9aa22220d1daa1e203d24e9843e
Eric Douglas <eric.douglas.mail@gmail.com>
2020-07-21 07:27:36 -0300

    add: working version with log - repeated string erlang

--- hackerrank/interview-preparation-kit/warmup-challenges/repeated-string/erlang/repeat.erl
+++ hackerrank/interview-preparation-kit/warmup-challenges/repeated-string/erlang/repeat.erl
@@ -1,15 +1,17 @@
 -module(repeat).
 
--compile(export_all).
+-export([test/0]).
 
-%% [97] == "a"
-count_comparison(String, Acc) when String == [97] ->
+%% 97 == "a"
+count_comparison(String, Acc) when String == 97 ->
     Acc + 1;
 count_comparison(_, Acc) -> Acc.
 
-% count_comparison(<<"a">>, Acc) ->
-%     io:format("::: Acc"),
-%     erlang:display(Acc),
+% count_comparison(String, Acc) when String == 97 ->
+%     io:format("::: String "),
+%     erlang:display(String),
+%     io:format("::: Acc "),
+%     erlang:display(Acc + 1),
 %     Acc + 1;
 % count_comparison(El, Acc) ->
 %     io:format("::: El "),
@@ -24,15 +26,17 @@ repeated_string(String, Size) ->
     {PartialStringPart, _} = lists:split(PartialStringSize,
                                          String),
     FullString = lists:concat(CompleteStringPart ++
-                                  PartialStringPart),
-    io:format("::: FullString"),
+                                  [PartialStringPart]),
+    io:format("::: PartialStringPart "),
+    erlang:display(PartialStringPart),
+    io:format("::: FullString "),
     erlang:display(FullString),
     lists:foldl(fun count_comparison/2, 0, FullString).
 
 test_repeated_string() ->
     2 = repeated_string("dia", 7),
-    % 7 = repeated_string("aba", 10),
-    % 1000000000000 = repeated_string("a", 1000000000000),
+    7 = repeated_string("aba", 10),
+    10 = repeated_string("a", 10),
     pass.
 
 test() -> pass = test_repeated_string().
