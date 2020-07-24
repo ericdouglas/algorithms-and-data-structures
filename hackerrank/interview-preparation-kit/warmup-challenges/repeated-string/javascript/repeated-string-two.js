abc69a08ef0501e6a73db78ad18cf7d833e88668
Eric Douglas <eric.douglas.mail@gmail.com>
2020-07-24 08:21:36 -0300

    add: second version of repeated string in javascript

---
+++ hackerrank/interview-preparation-kit/warmup-challenges/repeated-string/javascript/repeated-string-two.js
@@ -0,0 +1,29 @@
+const repeatNTimes = (str, size) => {
+  const length = str.length
+  const response = { repeat: 0, fraction: size }
+
+  if (length <= size) {
+    response.repeat = Math.floor(size / length)
+    response.fraction = size % length
+  }
+
+  return response
+}
+
+countPattern = (str, pattern) => {
+  const regex = new RegExp(pattern, 'g')
+  const reducedString = str.replace(regex, '')
+
+  return str.length - reducedString.length
+}
+
+const repeatedString = (str, size) => {
+  const { repeat, fraction } = repeatNTimes(str, size)
+  const stringFraction = str.slice(0, fraction)
+  const patternOccurrenceInString = repeat ? countPattern(str, 'a') : ''
+  const patternOccurrenceInFraction = countPattern(stringFraction, 'a')
+
+  return patternOccurrenceInString * repeat + patternOccurrenceInFraction
+}
+
+module.exports = repeatedString
