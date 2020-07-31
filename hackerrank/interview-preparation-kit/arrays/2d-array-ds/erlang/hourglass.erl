b53dd702a3050fe0f55208003995a03d5807603e
Eric Douglas <eric.douglas.mail@gmail.com>
2020-07-31 08:24:50 -0300

    wip: hourglass in erlang

--- hackerrank/interview-preparation-kit/arrays/2d-array-ds/erlang/hourglass.erl
+++ hackerrank/interview-preparation-kit/arrays/2d-array-ds/erlang/hourglass.erl
@@ -2,16 +2,43 @@
 
 -export([test/0]).
 
-hourglass_sum(Arr) -> 19.
+create_pivot_list([Row | _] = Rows) ->
+    %% The -2 is because we need to have at least 3 columns and rows to form an hourglass
+    RowsLength = length(Rows) - 2,
+    ColumnsLeght = length(Row) - 2,
 
-test_hour_glass_sum() ->
+    [lists:seq(1, RowsLength)
+     || _ <- lists:seq(1, ColumnsLeght)].
+
+hourglass_sum(Arr) ->
+    PivotList = create_pivot_list(Arr),
+    19.
+
+test_hourglass_sum() ->
     Input = [[1, 1, 1, 0, 0, 0],
              [0, 1, 0, 0, 0, 0],
              [1, 1, 1, 0, 0, 0],
              [0, 0, 2, 4, 4, 0],
              [0, 0, 0, 2, 0, 0],
              [0, 0, 1, 2, 4, 0]],
-    19 = hourglass_sum(Input),
+    Result = 19,
+    Result = hourglass_sum(Input),
     pass.
 
-test() -> pass = test_hour_glass_sum().
+test_create_pivot_list() ->
+    Input = [[1, 1, 1, 0, 0, 0],
+             [0, 1, 0, 0, 0, 0],
+             [1, 1, 1, 0, 0, 0],
+             [0, 0, 2, 4, 4, 0],
+             [0, 0, 0, 2, 0, 0],
+             [0, 0, 1, 2, 4, 0]],
+    Result = [[1, 2, 3, 4],
+              [1, 2, 3, 4],
+              [1, 2, 3, 4],
+              [1, 2, 3, 4]],
+    Result = create_pivot_list(Input),
+    pass.
+
+test() ->
+    pass = test_hourglass_sum(),
+    pass = test_create_pivot_list().
