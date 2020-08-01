7ce5ad5564ccd8d01534320834ac48d652392724
Eric Douglas <eric.douglas.mail@gmail.com>
2020-08-01 08:33:46 -0300

    test: create_sum_list

--- hackerrank/interview-preparation-kit/arrays/2d-array-ds/erlang/hourglass.erl
+++ hackerrank/interview-preparation-kit/arrays/2d-array-ds/erlang/hourglass.erl
@@ -2,17 +2,33 @@
 
 -export([test/0]).
 
-create_pivot_list([Row | _] = Rows) ->
-    %% The -2 is because we need to have at least 3 columns and rows to form an hourglass
-    RowsLength = length(Rows) - 2,
-    ColumnsLeght = length(Row) - 2,
+%% {Row, Column} - pivot's position of the hourglass
+%% Arr - original matrix
+create_sum_list({Row, Column}, Arr) ->
+    [[-63, -34, -9, 12],
+     [-10, 0, 28, 23],
+     [-27, -11, -2, 10],
+     [9, 17, 25, 18]].
 
-    [lists:seq(1, RowsLength)
-     || _ <- lists:seq(1, ColumnsLeght)].
-
-hourglass_sum(Arr) ->
-    PivotList = create_pivot_list(Arr),
+hourglass_sum([Row | _] = Rows) ->
+    RowsLength = length(Rows),
+    ColumnsLength = length(Row),
+    PivotListRows = lists:seq(1, RowsLength - 2),
+    PivotListColumns = lists:seq(1, ColumnsLength - 2),
+    SumList = lists:map(fun (Row) ->
+                                lists:map(fun (Column) ->
+                                                  create_sum_list({Row, Column},
+                                                                  Rows)
+                                          end,
+                                          PivotListColumns)
+                        end,
+                        PivotListRows),
     19.
+
+    %% Since the hourglass is a 3x3 matrix, we can not use a pivot that starts
+    %% in the last two columns and rows
+
+    %% TODO: try to go directly - reduce the lists:seq(1, RowsLength) inside lists:seq(1, ColumnsLength)
 
 test_hourglass_sum() ->
     Input = [[1, 1, 1, 0, 0, 0],
@@ -25,20 +41,20 @@ test_hourglass_sum() ->
     Result = hourglass_sum(Input),
     pass.
 
-test_create_pivot_list() ->
-    Input = [[1, 1, 1, 0, 0, 0],
-             [0, 1, 0, 0, 0, 0],
-             [1, 1, 1, 0, 0, 0],
-             [0, 0, 2, 4, 4, 0],
-             [0, 0, 0, 2, 0, 0],
+test_create_sum_list() ->
+    Input = [[-9, -9, -9, 1, 1, 1],
+             [0, -9, 0, 4, 3, 2],
+             [-9, -9, -9, 1, 2, 3],
+             [0, 0, 8, 6, 6, 0],
+             [0, 0, 0, -2, 0, 0],
              [0, 0, 1, 2, 4, 0]],
-    Result = [[1, 2, 3, 4],
-              [1, 2, 3, 4],
-              [1, 2, 3, 4],
-              [1, 2, 3, 4]],
-    Result = create_pivot_list(Input),
+    Result = [[-63, -34, -9, 12],
+              [-10, 0, 28, 23],
+              [-27, -11, -2, 10],
+              [9, 17, 25, 18]],
+    Result = create_sum_list({6, 6}, Input),
     pass.
 
 test() ->
     pass = test_hourglass_sum(),
-    pass = test_create_pivot_list().
+    pass = test_create_sum_list().
