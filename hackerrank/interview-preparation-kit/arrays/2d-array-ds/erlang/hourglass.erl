00608fd7cd3b1200b2e13887471ab28ebabcad6e
Eric Douglas <eric.douglas.mail@gmail.com>
2020-08-01 19:40:25 -0300

    finish: 2D Array - DS :tada:

--- hackerrank/interview-preparation-kit/arrays/2d-array-ds/erlang/hourglass.erl
+++ hackerrank/interview-preparation-kit/arrays/2d-array-ds/erlang/hourglass.erl
@@ -2,33 +2,40 @@
 
 -export([test/0]).
 
-%% {Row, Column} - pivot's position of the hourglass
-%% Arr - original matrix
-create_sum_list({Row, Column}, Arr) ->
-    [[-63, -34, -9, 12],
-     [-10, 0, 28, 23],
-     [-27, -11, -2, 10],
-     [9, 17, 25, 18]].
-
-hourglass_sum([Row | _] = Rows) ->
-    RowsLength = length(Rows),
-    ColumnsLength = length(Row),
-    PivotListRows = lists:seq(1, RowsLength - 2),
-    PivotListColumns = lists:seq(1, ColumnsLength - 2),
-    SumList = lists:map(fun (Row) ->
-                                lists:map(fun (Column) ->
-                                                  create_sum_list({Row, Column},
-                                                                  Rows)
-                                          end,
-                                          PivotListColumns)
-                        end,
-                        PivotListRows),
-    19.
-
+create_sum_list({RowsLength, ColumnsLength}, Arr) ->
     %% Since the hourglass is a 3x3 matrix, we can not use a pivot that starts
     %% in the last two columns and rows
+    PivotPositionList = [{X, Y}
+                         || X <- lists:seq(1, RowsLength - 2),
+                            Y <- lists:seq(1, ColumnsLength - 2)],
+    HourglassPattern = [{0, 0},
+                        {0, 1},
+                        {0, 2},
+                        {1, 1},
+                        {2, 0},
+                        {2, 1},
+                        {2, 2}],
+    lists:map(fun ({RowPosition, ColumnPosition}) ->
+                      lists:foldl(fun ({RowShift, ColumnShift}, Acc) ->
+                                          Row = lists:nth(RowPosition +
+                                                              RowShift,
+                                                          Arr),
+                                          Element = lists:nth(ColumnPosition +
+                                                                  ColumnShift,
+                                                              Row),
+                                          Acc + Element
+                                  end,
+                                  0,
+                                  HourglassPattern)
+              end,
+              PivotPositionList).
 
-    %% TODO: try to go directly - reduce the lists:seq(1, RowsLength) inside lists:seq(1, ColumnsLength)
+hourglass_sum([FirstRow | _] = Rows) ->
+    RowsLength = length(Rows),
+    ColumnsLength = length(FirstRow),
+    SumList = create_sum_list({RowsLength, ColumnsLength},
+                              Rows),
+    lists:max(SumList).
 
 test_hourglass_sum() ->
     Input = [[1, 1, 1, 0, 0, 0],
@@ -48,10 +55,22 @@ test_create_sum_list() ->
              [0, 0, 8, 6, 6, 0],
              [0, 0, 0, -2, 0, 0],
              [0, 0, 1, 2, 4, 0]],
-    Result = [[-63, -34, -9, 12],
-              [-10, 0, 28, 23],
-              [-27, -11, -2, 10],
-              [9, 17, 25, 18]],
+    Result = [-63,
+              -34,
+              -9,
+              12,
+              -10,
+              0,
+              28,
+              23,
+              -27,
+              -11,
+              -2,
+              10,
+              9,
+              17,
+              25,
+              18],
     Result = create_sum_list({6, 6}, Input),
     pass.
 
