b49e2a372dc8cf28512e783bcb9655c0a4bebe2b
Eric Douglas <eric.douglas.mail@gmail.com>
2020-08-02 09:00:41 -0300

    add: createPivotList function

--- hackerrank/interview-preparation-kit/arrays/2d-array-ds/javascript/hourglass.js
+++ hackerrank/interview-preparation-kit/arrays/2d-array-ds/javascript/hourglass.js
@@ -1,3 +1,31 @@
-const hourglass = () => 19
+const createPivotList = (rows, columns) => {
+  const pivotList = Array.from(new Array(rows), (_, rowIndex) => {
+    return Array.from(new Array(columns), (_, columnIndex) => [
+      rowIndex,
+      columnIndex,
+    ])
+  }).flat()
 
-module.exports = { hourglass }
+  return pivotList
+}
+
+const createSumList = ([rowsLength, columnsLength], list) => {
+  // Since the hourglass is a 3x3 matrix, we can not use a pivot that starts
+  // in the last two columns and rows
+  const pivotPositionList = createPivotList(rowsLength - 2, columnsLength - 2)
+}
+
+const verifyMatrixSize = list => {
+  const rows = list.length
+  const columns = list[0].length
+
+  return [rows, columns]
+}
+
+const hourglass = list => {
+  const [rows, columns] = verifyMatrixSize(list)
+  const sumList = createSumList([rows, columns], list)
+  return 19
+}
+
+module.exports = { hourglass, verifyMatrixSize, createSumList, createPivotList }
--- hackerrank/interview-preparation-kit/arrays/2d-array-ds/javascript/hourglass.test.js
+++ hackerrank/interview-preparation-kit/arrays/2d-array-ds/javascript/hourglass.test.js
@@ -1,8 +1,8 @@
-const { hourglass } = require('./hourglass')
+const { hourglass, createSumList, createPivotList } = require('./hourglass')
 
 describe('2d array DS - hourglass', () => {
-  it('should return the max hourglass sum value', () => {
-    const input = [
+  it('should return the max hourglass sum value - #hourglass', () => {
+    const matrix = [
       [1, 1, 1, 0, 0, 0],
       [0, 1, 0, 0, 0, 0],
       [1, 1, 1, 0, 0, 0],
@@ -11,6 +11,25 @@ describe('2d array DS - hourglass', () => {
       [0, 0, 1, 2, 4, 0],
     ]
 
-    expect(hourglass(input)).toBe(19)
+    expect(hourglass(matrix)).toBe(19)
+  })
+
+  it('should return a list with all hourglasses summed - #createSumList', () => {
+    //     const matrix = [
+    //   [1, 2, 3, 4],
+    //   [2, 1, 0, 1],
+    //   [4, 1, 0, 5],
+    //   [7, 4, 2, 5],
+    // ]
+    // expect(createSumList([3, 3], matrix)).toEqual([[0, 0]])
+  })
+
+  it('should return a list with pivots position - #createPivotList', () => {
+    expect(createPivotList(2, 2)).toEqual([
+      [0, 0],
+      [0, 1],
+      [1, 0],
+      [1, 1],
+    ])
   })
 })
